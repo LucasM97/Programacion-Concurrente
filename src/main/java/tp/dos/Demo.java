631af55540ac2e1ed942400431fecd5e9ea2fd29
Lucas Montaño <lucasmo24@gmail.com>
2023-09-13 15:34:54 -0300

    Actualizando tp2

--- src/main/java/tp/dos/Demo.java
+++ src/main/java/tp/dos/Demo.java
@@ -1,5 +1,7 @@
 package tp.dos;
 
+import java.util.Scanner;
+
 public class Demo {
     public static void main(String[] args) {
         //Ejercicio 1
@@ -11,13 +13,34 @@ public class Demo {
 
         //Ejercicio 3
         System.out.println("################## Movimiento del Caballo en Ajedrez ##################");
+        int x, y;
 
-        MatrizCuadrada<String> tablero = new TableroAjedrez(2,4);
+        x = ingresarEntero("Ingrese posicion en x (entre 1 y 10)");
+
+        y = ingresarEntero("Ingrese posicion en y (entre 1 y 10)");
+
+        MatrizCuadrada<String> tablero = new TableroAjedrez(x,y);
 
         //Ejercicio 6
         System.out.println("\n###################### Patron de relog de arena #######################");
         MatrizCuadrada<Character> figura = new Patron(6);
 
         System.out.println(figura);
+    }
+
+    public static int ingresarEntero(String mensaje) {
+        int dato;
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            try {
+                System.out.println("\n" + mensaje + "\n");
+                dato = scanner.nextInt();
+                if (dato < 11 && dato > 0) return dato;
+                else System.out.println("\nERROR: El valor ingresado no es valido\n");
+            } catch (Exception e) {
+                System.out.println("\nERROR: El valor ingresado no es valido\n");
+                scanner.nextLine();
+            }
+        }
     }
 }
