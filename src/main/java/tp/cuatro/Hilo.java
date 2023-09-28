0d108deb51a48bab4f998b4f1554ad7d0a4cd1dc
Lucas Montaño <lucasmo24@gmail.com>
2023-09-28 20:25:07 -0300

    Actualizando Hilo

--- src/main/java/tp/cuatro/Hilo.java
+++ src/main/java/tp/cuatro/Hilo.java
@@ -10,15 +10,13 @@ public class Hilo extends Thread{
     }
 
     public void run() {
-        synchronized (matrizA) {
-            int fila[] = new int[Matriz.FILAS];
+        int fila[] = new int[Matriz.FILAS];
 
-            for (int i = 0; i < Matriz.FILAS; i++) {
-                for (int j = 0; j < Matriz.COLUMNAS; j++) {
-                    fila[i] = matrizA.dato(i,j);
-                    matrizA.agregar(i,j,matrizB.dato(j,i));
-                    matrizB.agregar(j,i,fila[i]);
-                }
+        for (int i = 0; i < Matriz.FILAS; i++) {
+            for (int j = 0; j < Matriz.COLUMNAS; j++) {
+                fila[i] = matrizA.dato(i,j);
+                matrizA.agregar(i,j,matrizB.dato(j,i));
+                matrizB.agregar(j,i,fila[i]);
             }
         }
     }
