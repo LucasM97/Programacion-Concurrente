fc4553370e680b726cf87220f2f96fb713684434
Lucas Montaño <lucasmo24@gmail.com>
2023-09-28 17:34:39 -0300

    Actualizando gitignore

---
+++ src/main/java/tp/tres/clase/Principal.java
@@ -0,0 +1,16 @@
+package tp.tres.clase;
+
+public class Principal {
+    public static void main(String[] args) {
+        for (int i = 0; i < 100; i++) {
+            System.out.print("."+i+"X");
+        }
+
+        System.out.println();
+
+        for (int i = 0; i < 100; i++) {
+            System.out.print("."+i+"Y");
+        }
+    }
+
+}
---
+++ src/main/java/tp/tres/clase/PrintY.java
@@ -0,0 +1,2 @@
+package tp.tres.clase;public class PrintY {
+}
