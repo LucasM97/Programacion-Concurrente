92e8beff2a95704e5e741a77d8407c6690f845e7
Lucas Montaño <lucasmo24@gmail.com>
2023-10-27 21:54:01 -0300

    Actualizando E2 de TP6

--- src/main/java/tp/seis/tres/CalculoConcurrente.java
+++ src/main/java/tp/seis/tres/CalculoConcurrente.java
@@ -13,7 +13,7 @@ public class CalculoConcurrente extends Operaciones implements Runnable {
         this.numeroFila = numeroFila;
     }
 
-    public void run() {
+    public synchronized void run() {
         int multiplicacion;
         for (int i = 0; i < matrizB.getColumnas(); i++) {
             multiplicacion = 0;
